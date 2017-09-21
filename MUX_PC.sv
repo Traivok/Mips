5bbc8cdd6fcee6c0df9faf2a3e778a583bf43804
Gabriela Correia Holanda Freitas <gchf@cin.ufpe.br>
2017-09-21 20:27:49 -0300

    funcionando

--- MUX_PC.sv
+++ MUX_PC.sv
@@ -1,6 +1,7 @@
 module MUX_PC(input logic [31:0] ALUresult, // PC + 4
 				input logic [31:0] ALUout, // jmp address
-				input logic [31:0] instrShiftLeft,
+				input logic [31:0] instrShiftLeft, // 26 bits menos significativos do IR << 2 e concatenados com os
+				// 4 bits mais significativos do PC incrementado, que é a origem quando a instrução é um jump
 				input logic [1:0] PCsource,
 				input logic Clk, input logic PCwrite, 
 				input logic PCwriteCOND,
