e0b719a880a43d168cd441405f11828f2f67d3f1
Gabriela Correia Holanda Freitas <gchf@cin.ufpe.br>
2017-09-22 15:05:26 -0300

    Control

--- Control.sv
+++ Control.sv
@@ -1,20 +1,19 @@
 module Control( 
-				
 				input logic Clk,
 				input logic [5:0] Op,
 				
 				output logic PCWriteCond, 
-				output logic PCWrite, 
+				output logic PCWrite, 				// ativo em 1
 				output logic IorD,
 				output logic MemRead,
-				output logic MemWrite,
+				output logic wr, 					// memory write/read control
 				output logic MemtoReg, 
-				output logic IRWrite, 
+				output logic IRWrite, 				// Instruction register write controla a escrita no registrador de instruç˜oes.
 				output logic [1:0] PCSource,
 				output logic [1:0] ALUOp,
 				output logic [1:0] ALUSrcB
 				output logic ALUSrcA,
-				output logic RegWrite,
+				output logic RegWrite,				// write registers control
 				output logic RegDst,
 				output logic [7:0] StateOut
 			  );
@@ -36,79 +35,40 @@ module Control(
 		
 	/* BEGIN OF STATE SECTION */
 		logic [7:0] State;
-		enum { Init, Fetch_Pre_Memory, Memory_Delay1, Memory_Delay2, Fetch_Pos_Memory } PossibleStates;		
+		
+		parameter init = 8'd0, funct_state = 8'h0x0, 
+		rte = 8'h0x10, addi = 8'h0x8, addu = 8'h0x9, 
+		andi = 8'h0xc, beq = 8'h0x4, bne = 8'h0x5, 
+		lbu = 8'h0x24, lhu = 8'h0x25, lui = 8'h0xf, 
+		lw = 8'h0x23, sb = 8'h0x28, sh = 8'h0x29, 
+		slti = 8'h0xa, sw = 8'h0x2b, sxori = 8'h0xe, 
+		j = 8'h0x2, jal = 8'h0x3;
+		
 		initial State = Init;
-	/* END OF STATE SECTION */
+	/* END OF STATE SECTION */	
 		
-		always_ff@(posedge Clk)
+		always_comb@(posedge Clk)
 		begin
 			case (State)
 				Init:
 				begin
-					StateOut <= State;
-					State <= Fetch_Pre_Memory;
+					
+					
 				end
 				
-				Fetch_Pre_Memory:
+				funct_state:
 				begin
-					StateOut <= State;
-					State <= Memory_Delay1;
+				
 				end
 				
-				Memory_Delay1:
+				rte:
 				begin
-					StateOut <= State;
-					State <= Memory_Delay2;
+					PCWrite = 1'b1;
+					wr = 1'b1;
+					PCWriteCond = 1'b0;
+					
 				end
 				
-				Memory_Delay2:
-				begin
-					StateOut <= State;
-					State <= Fetch_Pos_Memory;
-				end
-								
-				Fetch_Pos_Memory:
-				begin
-					StateOut <= State;
-					State <= Fetch_Pre_Memory;
-				end
-				
-			endcase	
-		end		
-		
-		
-		always_comb
-		begin
-			case (State)
-				Init:
-				begin
-					StateOut <= State;
-					State <= Fetch_Pre_Memory;
-				end
-				
-				Fetch_Pre_Memory:
-				begin
-					StateOut <= State;
-					State <= Memory_Delay1;
-				end
-				
-				Memory_Delay1:
-				begin
-					StateOut <= State;
-					State <= Memory_Delay2;
-				end
-				
-				Memory_Delay2:
-				begin
-					StateOut <= State;
-					State <= Fetch_Pos_Memory;
-				end
-								
-				Fetch_Pos_Memory:
-				begin
-					StateOut <= State;
-					State <= Fetch_Pre_Memory;
-				end	
 		end
 	
 endmodule : Control;
