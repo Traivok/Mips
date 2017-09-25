3626c5c989dec1b36aa086dc2bdbdfcbcd391381
Gabriela Correia Holanda Freitas <gchf@windows.cin.ufpe.br>
2017-09-25 15:15:19 -0300

    sign extend

--- SignExtend.sv
+++ SignExtend.sv
@@ -1,3 +1,3 @@
 module SignExtend(input logic [15:0] in_vector, output logic [31:0] out_vector);
-	assign out_vector = {{16{in_vector[15]}}, {in_vector[15:0]}};
-endmodule
+	assign out_vector = {{16{in_vector[0]}}, {in_vector[15:0]}};
+endmodule 
