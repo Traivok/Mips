f670902b49968a7f3861f56a53557dc3587b1d54
José Ricardo A. Figueirôa <jraf@cin.ufpe.br>
2017-10-04 14:41:45 -0300

    Mux32bit_8x1

--- MUX.sv
+++ MUX.sv
@@ -94,4 +94,25 @@ module Mux32bit_2x1 (
 	end
 
 endmodule : Mux32bit_2x1
+
+module Mux32bit_8x1(
+	input logic [2:0] sel,
+	input logic [31:0] in0, in1, in2, in3, in4, in5, in6, in7,
+	output logic [31:0] out );
 	
+	always_comb
+	begin
+		case (sel)			
+			3'b000: out <= in0;
+			3'b001:	out <= in1;
+			3'b010:	out <= in2;
+			3'b011:	out <= in3;
+			3'b100:	out <= in4;
+			3'b101:	out <= in5;
+			3'b110:	out <= in6;
+			3'b111:	out <= in7;
+			default: out <= 32'd1234567890;
+		endcase
+	end
+	
+endmodule : Mux32bit_8x1
