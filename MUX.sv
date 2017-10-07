f51bbe9a2712b3f7259f8eb5a29e3032e22cd3bb
Traivok <jraf@cin.ufpe.br>
2017-10-07 19:14:55 -0300

    New mux 5bits 4x1

--- MUX.sv
+++ MUX.sv
@@ -116,3 +116,43 @@ module Mux32bit_8x1(
 	end
 	
 endmodule : Mux32bit_8x1
+
+module Mux32bits_4x2(
+	input logic [1:0] sel,
+	input logic [4:0] in0, in1, in2, in3,
+	output logic [4:0] out);
+	
+	always_comb
+	begin
+		
+		case(sel)
+		
+			2'b00:
+			begin
+				out <= in0;	
+			end
+			
+			2'b01:
+			begin
+				out <= in1;
+			end
+			
+			2'b10:
+			begin
+				out <= in2;
+			end
+		
+			2'b11:
+			begin
+				out <= in3;
+			end
+			/*
+			default:
+			begin
+				out <= 32'd1234567890;
+			end*/
+				
+		endcase
+	end
+	
+endmodule : Mux32bits_4x2
