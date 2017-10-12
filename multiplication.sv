860b28255f12955bb4014ef845d86054b6973b1d
Traivok <jraf@cin.ufpe.br>
2017-10-12 16:05:05 -0300

    Multiplication module done

--- multiplication.sv
+++ multiplication.sv
@@ -1,39 +1,43 @@
-module multiplication(input logic Clk, work, reset, input logic [31:0] lhs, rhs, output logic [63:0] result, output logic endSignal, output logic [4:0] counter);
-	/*
-	logic [31:0] multiplier, multiplicand;
-	assign result = {multiplier, multiplicand} ;
+module multiplication(input logic Clk, work, reset, input logic [31:0] lhs, rhs, output logic [63:0] result, output logic endSignal, output logic [5:0] counter);
 	
+	logic [31:0] multiplier, multiplicand;
+
 	always_ff@(posedge Clk or posedge reset)
 	begin
 			if (reset)
 			begin
 				endSignal = 1;
-			end
-			else if (start)
-			begin
+				result = 64'd0;
 				multiplier = lhs;
 				multiplicand = rhs;
-				result = 64'd0;
-				counter = 5'd0;
-				endSignal = 0;
 			end
-			
-			for ( counter = counter; counter != 5'd31; counter++ )
+			else
 			begin
-				if (multiplier[0] == 1)
+				if (~work)
 				begin
-					result = multiplicand + result; 
+					endSignal = 1;
+					result = 64'd0;
+					multiplier = lhs;
+					multiplicand = rhs;
 				end
-				
-				multiplicand = multiplicand << 1;
-				multiplier = multiplier >> 1;
-				
+				else
+				begin
+					for (counter = 6'd0; counter < 6'd32; counter++)
+					begin
+						if (multiplicand[0] == 31'd0)
+						begin
+							result = multiplicand + result;
+						end
+						
+						multiplicand = multiplicand << 1;
+						multiplier = multiplier >> 1;
+						
+						if (counter == 6'd32) endSignal = 1;
+						else endSignal = 0;						
+						
+					end
+				end				
 			end
-			
-			if (counter == 5'd31)
-			begin
-				endSignal = 1;
-			end						
-	end
-	*/
+		end
+	
 endmodule : multiplication 
