a64519f1d10e6ea2b88a12a5eef2ef87887f09e5
Traivok <jraf@cin.ufpe.br>
2017-10-19 01:32:01 -0200

    sb still not working

--- MemWrapper.sv
+++ MemWrapper.sv
@@ -1,6 +1,6 @@
 module MemWrapper(input logic [31:0] MemData, input logic [31:0] Address, input logic [31:0] value, output logic [31:0] HalfWord, output logic [31:0] Byte);
 
-assign HalfWord [31:00] = { value [15:00], MemData[15:00] };
-assign Byte [31:00] = { value [07:00], MemData[23:00] };
+assign HalfWord [31:00] = { MemData[31:16], value [15:00] };
+assign Byte [31:00] = { MemData[31:08], value [07:00] };
 
 endmodule : MemWrapper
